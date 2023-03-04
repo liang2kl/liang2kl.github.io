9a264ae15796a4f1673506bead1d1f46120a1327
Liang Yesheng <liang2kl@outlook.com>
2023-03-04 14:40:30 +0800

    Update sources

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -6,6 +6,7 @@ const sh = require('shelljs');
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
+
     sh.cp('-R', sourcePath, destPath)
+    sh.touch(destPath + "/.nojekyll")
 };
